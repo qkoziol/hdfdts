db10db90b8b61ba73a9d3688924e4dd700bd2f83
chilan <chilan@hdfgroup.org>
2006-08-15 01:02:27 +0000

    *** empty log message ***

--- h5check/test/testgen.c
+++ h5check/test/testgen.c
@@ -24,7 +24,7 @@
 
 #define NUM_GROUPS 512 /* number of groups for linear structure */
 
-#define HEIGHT 5 /* height of group trees (recursion levels) */
+#define HEIGHT 2 /* height of group trees (recursion levels) */
 
 /* type of group structure */
 #define HIERARCHICAL 0
@@ -48,41 +48,60 @@
 #define LEFT 0
 #define RIGHT 1
 
+/* numerical basic types */
+#define NTYPES 9
+#define NATIVE_TYPE(i)  \
+    (i==0)?H5T_NATIVE_CHAR: \
+    (i==1)?H5T_NATIVE_SHORT:    \
+    (i==2)?H5T_NATIVE_INT:  \
+    (i==3)?H5T_NATIVE_UINT: \
+    (i==4)?H5T_NATIVE_LONG: \
+    (i==5)?H5T_NATIVE_LLONG:    \
+    (i==6)?H5T_NATIVE_FLOAT:    \
+    (i==7)?H5T_NATIVE_DOUBLE:   \
+        H5T_NATIVE_LDOUBLE
+
+char *ntype_dset[]={"char","short","int","uint","long","llong",
+    "float","double","ldouble"};
+
 int nerrors=0;
+
 
 
 /*
 * This function creates a file using default properties.
 */
 
-hid_t create_file(char *fname, char *options)
+static hid_t create_file(char *fname, char *options)
 {
-       hid_t fid;
+    hid_t fid;
 
-       VRFY(isalnum(*fname),"filename");
+    VRFY(isalnum(*fname),"filename");
 
-       /* create a file */  
-       fid = H5Fcreate(fname, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
-       VRFY((fid>=0), "H5Fcreate");
+    /* create a file */  
+    fid = H5Fcreate(fname, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+    VRFY((fid>=0), "H5Fcreate");
 
-       return fid;
+    return fid;
 
 }
+
+
 
 /*
 * This function closes a file.
 */
 
-void close_file(hid_t fid, char *options)
+static void close_file(hid_t fid, char *options)
 {
+    herr_t ret;
 
-       herr_t ret;
-
-       /* close file */
-       ret = H5Fclose(fid);
-       VRFY((ret>=0), "H5Fclose");
+    /* close file */
+    ret = H5Fclose(fid);
+    VRFY((ret>=0), "H5Fclose");
 
 }
+
 
 
 /*
@@ -96,73 +115,74 @@ void close_file(hid_t fid, char *options)
 * All pointers are implemented as HARDLINKS.
 */
 
-void gen_group_struct(hid_t parent_id, char* prefix, int height, int option)
+static void gen_group_struct(hid_t parent_id, char* prefix, int height, int option)
 {
-       hid_t child_gid0, child_gid1; /* groups IDs */
-       herr_t ret;
+    hid_t child_gid0, child_gid1; /* groups IDs */
+    herr_t ret;
 
-       char gname0[64]; /* groups names */
-       char gname1[64];
-       char child_name[64];
+    char gname0[64]; /* groups names */
+    char gname1[64];
+    char child_name[64];
 
-       /* create child group 0 */
-       sprintf(gname0, "%s_%d", prefix, 0);
-       child_gid0 = H5Gcreate(parent_id, gname0, 0);
-       VRFY((child_gid0 >= 0), "H5Gcreate");
+    /* create child group 0 */
+    sprintf(gname0, "%s_%d", prefix, 0);
+    child_gid0 = H5Gcreate(parent_id, gname0, 0);
+    VRFY((child_gid0 >= 0), "H5Gcreate");
 
-       /* create child group 1 */
-       sprintf(gname1, "%s_%d", prefix, 1);
-       child_gid1 = H5Gcreate(parent_id, gname1, 0);
-       VRFY((child_gid1 >= 0), "H5Gcreate");
+    /* create child group 1 */
+    sprintf(gname1, "%s_%d", prefix, 1);
+    child_gid1 = H5Gcreate(parent_id, gname1, 0);
+    VRFY((child_gid1 >= 0), "H5Gcreate");
 
-        /* recursive calls for remaining levels */        
-       if( height > 1 ){
+    /* recursive calls for remaining levels */        
+    if( height > 1 ){
 
-           /* create subtree at child 0 */
-           gen_group_struct(child_gid0, gname0, height-1, option);
 
-           /* create subtree at child 1 */
-           gen_group_struct(child_gid1, gname1, height-1, option);
+        /* create subtree at child 0 */
+        gen_group_struct(child_gid0, gname0, height-1, option);
 
-           switch(option){
+        /* create subtree at child 1 */
+        gen_group_struct(child_gid1, gname1, height-1, option);
 
-           case HIERARCHICAL: /* binary tree already constructed */
-               break;
+        switch(option){
 
-           case MULTIPATH:
+        case HIERARCHICAL: /* binary tree already constructed */
+            break;
 
-               /* a third entry in child_gid1 points to a child of child_gid_0*/
-               ret = H5Glink2(child_gid0, strcat(gname0,"_0"), H5G_LINK_HARD, child_gid1, strcat(gname1, "_2"));
-               VRFY((ret>=0), "H5Glink2");
-               break;
+        case MULTIPATH:
+            /* a third entry in child_gid1 points to a child of child_gid_0*/
+            ret = H5Glink2(child_gid0, strcat(gname0,"_0"), H5G_LINK_HARD, child_gid1, strcat(gname1, "_2"));
+            VRFY((ret>=0), "H5Glink2");
+            break;
 
-           case CYCLICAL:
+        case CYCLICAL:
                
-               /* a third entry in child_gid1 points to its grandparent. However, if
-               grandparent is the root, the entry will point to child_gid1. */
-               sprintf(child_name, "%s_%d", gname1, 2);
-               ret = H5Glink2(parent_id, ".", H5G_LINK_HARD, child_gid1, child_name);
-               VRFY((ret>=0), "H5Glink2");
+            /* a third entry in child_gid1 points to its grandparent. However, if
+            grandparent is the root, the entry will point to child_gid1. */
+            sprintf(child_name, "%s_%d", gname1, 2);
+            ret = H5Glink2(parent_id, ".", H5G_LINK_HARD, child_gid1, child_name);
+            VRFY((ret>=0), "H5Glink2");
 
-               /* a fourth entry in child_gid1 points its parent */
-               sprintf(child_name, "%s_%d", gname1, 3);
-               ret = H5Glink2(child_gid1, ".", H5G_LINK_HARD, child_gid1, child_name);
-               VRFY((ret>=0), "H5Glink2");
-               break;
+            /* a fourth entry in child_gid1 points its parent */
+            sprintf(child_name, "%s_%d", gname1, 3);
+            ret = H5Glink2(child_gid1, ".", H5G_LINK_HARD, child_gid1, child_name);
+            VRFY((ret>=0), "H5Glink2");
+            break;
                
-           default:
-               VRFY(FALSE, "Invalid option");
-           }
+        default:
+            
+            VRFY(FALSE, "Invalid option");
+        }
+    }
 
-       }
+    /* close groups */
+    ret = H5Gclose(child_gid0);
+    VRFY((ret>=0), "H5Gclose");
 
-        /* close groups */
-       ret = H5Gclose(child_gid0);
-       VRFY((ret>=0), "H5Gclose");
-
-       ret = H5Gclose(child_gid1);
-       VRFY((ret>=0), "H5Gclose");
+    ret = H5Gclose(child_gid1);
+    VRFY((ret>=0), "H5Gclose");
 }
+
 
 
 /*
@@ -171,7 +191,7 @@ void gen_group_struct(hid_t parent_id, char* prefix, int height, int option)
 * The dataset placement criteria reverses at every level of the tree.
 */
 
-void gen_group_dataset(hid_t parent_id, char* prefix, int height, int data_child)
+static void gen_group_datasets(hid_t parent_id, char* prefix, int height, int data_child)
 {
 
     hid_t child_gid0, child_gid1, gid; /* groups IDs */
@@ -239,10 +259,10 @@ void gen_group_dataset(hid_t parent_id, char* prefix, int height, int data_child
 
         /* dataset placement criteria reversed */
         /* create subtree at child 0 */
-        gen_group_dataset(child_gid0, gname0, height-1, !data_child);
+        gen_group_datasets(child_gid0, gname0, height-1, !data_child);
 
         /* create subtree at child 1 */
-        gen_group_dataset(child_gid1, gname1, height-1, data_child);
+        gen_group_datasets(child_gid1, gname1, height-1, data_child);
     
     }
 
@@ -261,29 +281,29 @@ void gen_group_dataset(hid_t parent_id, char* prefix, int height, int data_child
 * The generated structure tests the format validation of a large number of groups.
 */
 
-void gen_linear_rec(hid_t parent_id, char *prefix, unsigned int level)
+static void gen_linear_rec(hid_t parent_id, char *prefix, unsigned int level)
 {
+    hid_t child_gid; /* group ID */
+    herr_t ret;
 
-       hid_t child_gid; /* group ID */
-       herr_t ret;
+    char gname[64]; /* group name */
 
-       char gname[64]; /* group name */
+    /* appending level to the name prefix */
+    sprintf(gname, "%s_%d", prefix, level);
 
-       /* appending level to the name prefix */
-       sprintf(gname, "%s_%d", prefix, level);
+    /* create new group within parent_id group */
+    child_gid = H5Gcreate(parent_id, gname, 0);
+    VRFY((child_gid>=0), "H5Gcreate");
 
-       /* create new group within parent_id group */
-       child_gid = H5Gcreate(parent_id, gname, 0);
-       VRFY((child_gid>=0), "H5Gcreate");
+    /* recursive call for the remaining levels */        
+    if( level > 1 )
+        gen_linear_rec(child_gid, prefix, level-1);
 
-       /* recursive call for the remaining levels */        
-       if( level > 1 )
-           gen_linear_rec(child_gid, prefix, level-1);
-
-        /* close group */
-       ret = H5Gclose(child_gid);
-       VRFY((ret>=0), "H5Gclose");
+    /* close group */
+    ret = H5Gclose(child_gid);
+    VRFY((ret>=0), "H5Gclose");
 }
+
 
 
 /*
@@ -292,8 +312,9 @@ void gen_linear_rec(hid_t parent_id, char *prefix, unsigned int level)
 * the format validation of a large number of groups.
 */
 
-void gen_linear(hid_t fid)
+static void gen_linear(hid_t fid)
 {
+
     hid_t gid; /* group ID */
     herr_t ret;
     char gname[64]; /* group name */
@@ -322,6 +343,7 @@ void gen_linear(hid_t fid)
 }
 
 
+
 /*
 *
 * This function generates a dataset per allowed dimensionality, i.e. one
@@ -329,7 +351,7 @@ void gen_linear(hid_t fid)
 * The option 'status' determines if data is to be written in the file.
 */
 
-void gen_rank_dataset(hid_t oid, int status)
+static void gen_rank_datasets(hid_t oid, int fill_dataset)
 {
 
     hid_t dset_id, dspace_id; /* HDF5 IDs */
@@ -365,7 +387,7 @@ void gen_rank_dataset(hid_t oid, int status)
         VRFY((dset_id>=0), "H5Dcreate");
         
         /* dataset is to be full */
-        if (status){
+        if (fill_dataset){
 
             /* allocate buffer and assigns data */
             buffer = (int *)malloc(powl(size,rank)*sizeof(int));
@@ -392,19 +414,93 @@ void gen_rank_dataset(hid_t oid, int status)
     }
 }
 
+
+
+/*
+*
+* This function generates a dataset per numerical datatype , i.e. one
+* dataset is char, another one is int, and so on. The option 'fill_dataset' determines
+* if data is to be written in the file.
+*/
+
+static void gen_basic_ntypes(hid_t oid, int fill_dataset)
+{
+
+    hid_t dset_id, dspace_id; /* HDF5 IDs */
+    hid_t datatype;
+    hsize_t dims[RANK]; 
+    herr_t ret;
+    int *int_buffer;
+    float *float_buffer;
+
+    long int i;
+
+    for(i=0; i < RANK; i++)
+        dims[i] = SIZE;
+
+    /* create dataspace */
+    dspace_id = H5Screate_simple(RANK, dims, NULL);
+    VRFY((dspace_id>=0), "H5Screate_simple");
+
+    /* allocate integer buffer and assigns data */
+    int_buffer = (int *)malloc(powl(SIZE,RANK)*sizeof(int));
+    VRFY((int_buffer!=NULL), "malloc");
+    
+    /* allocate float buffer and assigns data */
+    float_buffer = (float *)malloc(powl(SIZE,RANK)*sizeof(float));
+    VRFY((float_buffer!=NULL), "malloc");
+
+    /* fill buffer with data */
+    for (i=0; i < powl(SIZE,RANK); i++)
+        float_buffer[i] = int_buffer[i] = i%SIZE;
+
+    /* iterate over numerical basic datatypes */            
+    for(i=0;i<NTYPES;i++){
+ 
+        /* create dataset */
+        dset_id = H5Dcreate(oid, ntype_dset[i], NATIVE_TYPE(i), dspace_id, H5P_DEFAULT);
+        VRFY((dset_id>=0), "H5Dcreate");
+
+        /* fill dataset with data */
+        if (fill_dataset){
+            /* since some datatype conversions are not allowed, select buffer with
+            compatible datatype class */
+            if(H5Tget_class(NATIVE_TYPE(i)) == H5T_INTEGER)
+                /* writes buffer into dataset */
+                ret = H5Dwrite(dset_id, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT, int_buffer);
+            else
+                /* writes buffer into dataset */
+                ret = H5Dwrite(dset_id, H5T_NATIVE_FLOAT, H5S_ALL, H5S_ALL, H5P_DEFAULT, float_buffer);
+
+            VRFY((ret>=0), "H5Dwrite");
+        }
+
+        /* close dataset */
+        ret = H5Dclose(dset_id);
+        VRFY((ret>=0), "H5Dclose");
+    }
+
+    /* close dataspace */
+    ret = H5Sclose(dspace_id);
+    VRFY((ret>=0), "H5Sclose");
+}
+
+
+
 /* main function of test generator */
 
 int main(int argc, char *argv[])
 {
+
     hid_t fid;
 
     char *fname[]={"root.h5","linear.h5","hierarchical.h5","multipath.h5","cyclical.h5",
-        "dset_empty.h5","dset_full.h5","group_dsets.h5"}; /* file names */
+        "rank_dsets_empty.h5","rank_dsets_full.h5","group_dsets.h5","basic_ntypes.h5"}; /* file names */
 
     unsigned i=0;
 
     /* initial message */
-    printf("Generating test files...");
+    printf("Generating test files for H5check...");
     fflush(stdout);
     
     /* root group is created along with the file */
@@ -433,22 +529,27 @@ int main(int argc, char *argv[])
 
     /* create an empty dataset for each possible rank */
     fid = create_file(fname[i++], "");
-    gen_rank_dataset(fid, EMPTY);
+    gen_rank_datasets(fid, EMPTY);
     close_file(fid, "");
 
     /* create a full dataset for each possible rank */
     fid = create_file(fname[i++], "");
-    gen_rank_dataset(fid, FULL);
+    gen_rank_datasets(fid, FULL);
     close_file(fid, "");
 
     /* create a tree like structure where some groups are empty
     while others contain a dataset */
     fid = create_file(fname[i++], "");
-    gen_group_dataset(fid, GROUP_PREFIX, HEIGHT, RIGHT);
+    gen_group_datasets(fid, GROUP_PREFIX, HEIGHT, RIGHT);
     close_file(fid, "");
+
+    fid = create_file(fname[i++], "");
+    gen_basic_ntypes(fid, FULL);
+    close_file(fid, "");
+
     
     /* successful completion message */
-    printf("\rTest files generation successful!\n");
+    printf("\rTest files generation for H5check successful!\n");
 
     return 0;
 
