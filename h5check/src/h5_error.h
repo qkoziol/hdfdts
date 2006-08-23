daf8a5075eb34df332a732821407cd228d425441
Vailin Choi <vchoi@hdfgroup.org>
2006-08-23 08:51:39 -0500

    Purpose:To check in the initial version of h5checker
    Description:Initial version of h5checker
    
    Solution:
    
    Platforms tested:heping
    
    Misc. update:

---
+++ h5check/src/h5_error.h
@@ -0,0 +1,19 @@
+#define H5E_NSLOTS      32      /* number of slots in an error stack */
+
+/* Information about an error */
+typedef struct H5E_error_t {
+    const char  *func_name;             /* function in which error occurred   */
+    const char  *desc;                  /* supplied description      */
+    haddr_t	address;		/* address where error occurs */
+} H5E_error_t;
+
+
+/* An error stack */
+typedef struct H5E_t {
+    int nused;                  	/* num slots currently used in stack  */
+    H5E_error_t slot[H5E_NSLOTS];       /* array of error records */
+} H5E_t;
+
+/* the current error stack */
+H5E_t	H5E_stack_g[1];
+#define	H5E_get_my_stack()	(H5E_stack_g+0)
