1ec008d9012c03e485a1a07f95242b7a45cb7721
hdftest <hdftest@hdfgroup.org>
2008-07-21 17:18:32 -0500

    Return error code from h5compat.sh if test fails.

--- bin/h5compat.sh
+++ bin/h5compat.sh
@@ -5,9 +5,16 @@ if [ -r ~hdftest/snapshots-h5compat/.h5compatrc ]; then
    cp ~hdftest/snapshots-h5compat/.h5compatrc ./format
 fi
 
-cd ./api
-./check_api.sh
-cd ../format
-./check_format.sh
-cd ..
+if (cd ./api &&
+    ./check_api.sh &&
+    cd ../format &&
+    ./check_format.sh &&
+    cd ..); then
+    :   #continue
+else
+    errcode=$?
+    echo "===== Exit h5compat.sh with status=$errcode ====="
+    echo ""
+    exit $errcode
+fi
 
