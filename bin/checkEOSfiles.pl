dd3543cefecaf35eb3b0d9d9a6eddcef5627100e
Larry Knox <lrknox@hdfgroup.org>
2009-03-05 16:40:22 -0600

    Check in changes to add daily sampling of files from NASA ftp servers.

--- bin/checkEOSfiles.pl
+++ bin/checkEOSfiles.pl
@@ -13,19 +13,20 @@ chomp($HOST_NAME);
 if ("$OP_CONFIGURE" ne "") {
    $HOST_NAME = $HOST_NAME.$OP_CONFIGURE;
 }
-my $PREFIX = "TestDir/$HOST_NAME";
+#my $PREFIX = "TestDir/$HOST_NAME";
+my $PREFIX = "";
 my $SELECTED = "/mnt/scr1/NASA/selected";
 my $CORRUPTED = "/mnt/scr1/NASA/corrupted/MLS";
-my $CORREPACKED18 = "$PREFIX/corrupted-repacked18/MLS";
-my $CORREPACKED19 = "$PREFIX/corrupted-repacked19/MLS";
-my $REPACKED18 = "$PREFIX/repacked18/";
-my $REPACKED19 = "$PREFIX/repacked19/";
+#my $CORREPACKED18 = "$PREFIX/corrupted-repacked18/MLS";
+#my $CORREPACKED19 = "$PREFIX/corrupted-repacked19/MLS";
+#my $REPACKED18 = "$PREFIX/repacked18/";
+#my $REPACKED19 = "$PREFIX/repacked19/";
 #my $COPIES = "$PREFIX/copies";
-my $COPYDIR = "$PREFIX/copies";
-#my $CORREPACKED18 = "corrupted-repacked18/MLS";
-#my $CORREPACKED19 = "corrupted-repacked19/MLS";
-#my $REPACKED18 = "repacked18";
-#my $REPACKED19 = "repacked19";
+my $COPYDIR ="copies";
+my $CORREPACKED18 = "corrupted-repacked18/MLS";
+my $CORREPACKED19 = "corrupted-repacked19/MLS";
+my $REPACKED18 = "repacked18";
+my $REPACKED19 = "repacked19";
 #my $COPYDIR = "/mnt/scr1/NASA/copies";
 my $OOPS = "";
 my $HDF5_DIR = "/mnt/scr1/pre-release/hdf5/";
@@ -38,7 +39,7 @@ my $STR19 = $HDF5_DIR."v190/".$HOST_NAME."-strict";
 my $NSTR19 = $HDF5_DIR."v190/".$HOST_NAME."-nostrict";
 #my $H5CHECK =  $HDF5_DIR."v180-compat/".$HOST_NAME."/bin/h5check";
 my @dirs = ($STR16, $NSTR16, $STR18, $NSTR18, $STR19, $NSTR19);
-my @binaries = ("h5ls", "h5dump -H", "h5stat", "h5copy");
+my @binaries = ("h5ls", "h5dump", "h5stat", "h5copy");
 my $retval = 0;
 
 sub evaluate_result {
@@ -60,12 +61,53 @@ sub evaluate_result {
    }
 }
 
+my $check_commands = {  h5ls => sub { my $dir = shift;
+                                      my $directory = shift;
+                                      my $testfile = shift;
+                                      my $cmd = "$dir/bin/h5ls $directory/$testfile";
+                                      return $cmd;},
+                        h5dump => sub { my $dir = shift;
+                                      my $directory = shift;
+                                      my $testfile = shift;
+                                      my $cmd = "$dir/bin/h5dump -H $directory/$testfile";
+                                      return $cmd;},
+                        h5stat => sub { my $dir = shift;
+                                      my $directory = shift;
+                                      my $testfile = shift;
+                                      my $cmd = "$dir/bin/h5stat $directory/$testfile";
+                                      return $cmd;},
+                        h5copy => sub { my $dir = shift;
+                                      my $directory = shift;
+                                      my $testfile = shift;
+                                      my $COPIES = "";
+                                      if ($dir eq "$NSTR18" || $dir eq "$STR18" ) {
+                                         $COPIES = "$COPYDIR/v180";
+                                      } elsif ($dir eq "$NSTR19" || $dir eq "$STR19") {
+                                         $COPIES = "$COPYDIR/v190";
+                                      }
+                                      if (-e "$COPIES/$testfile") {
+                                          my $command = `rm "$COPIES/$testfile"`;
+                                          my $output = `$command`;
+                                          sleep(10);
+                                      }
+                                      print "Running h5copy on $directory/$testfile\n";
+                                      my $cmd = "$dir/bin/h5copy -i $directory/$testfile -o $COPIES/$testfile -s \"/\" -d \"/COPY\" ";}
+                     };
+
+#for future reference:  check_commands above is a hash of anonymous subroutines
+#(or maybe a reference to a hash of anonymous subroutines, or some such thing).
+#I created it to eliminate an impossible if statement and to avoid a 4 part
+#if/else series to create the correct command for each program.  It is invoked
+#below by $check_commands->{"$program"}->(). 
+
+
 sub check_dir {
    my $dir = shift;
    my $directory = shift;
    my $RET = shift;
    my $result;
-   my $COPIES = "";
+   my $cmd;
+
    #system("setenv LD_LIBRARY_PATH $dir/lib");
    #Add the current library to the previous LD_LIBRARY_PATH
    $ENV{LD_LIBRARY_PATH} = "$ENV_LD_LIBRARY_PATH:$dir/lib";
@@ -78,59 +120,40 @@ sub check_dir {
       my $testfile = $_;
       print "checking $testfile\n";
       foreach my $program (@binaries) {
-      my $cmd;
-      if ($dir eq "$NSTR18" || $dir eq "$STR18" ) {
-         $COPIES = "$COPYDIR/v180";
-      } elsif ($dir eq "$NSTR19" || $dir eq "$STR19") {
-         $COPIES = "$COPYDIR/v180";
-      }
+         print "Program to test is $program with expected return value $RET.\n";
+         if (-e "$dir/bin/$program") {
+            $cmd = $check_commands->{"$program"}->($dir, $directory, $testfile);
+
+            # v1.8 h5stat returns 0 even if it fails to open the file.
+            if ($program eq "h5stat" && ($dir eq "$NSTR18" || $dir eq "$STR18"
+                                        || $dir eq "$NSTR19" || $dir eq "$STR19")) {
+               open(STDERR,">h5stat_stderr.txt");
+            }
+
+            $_ = `$cmd`;
+            $result = $? >> 2;
+
+            #check result if v1.8 h5stat and result == 0; remove err file.
+            if(-e "h5stat_stderr.txt") { 
+               if ($result == 0) {
+                  $_ = $_.`cat h5stat_stderr.txt`;
+                  if (/Unable to traverse object/) {
+                     $result = 1;
+                  }
+               }
+               `rm h5stat_stderr.txt`;      
+            }
+
+            #print $output;
+            evaluate_result ($RET, $result, $directory, $testfile, $dir, $program) ;
+         } else {
+            print "$program not found in $dir.\n";
+         }
    #   my $cmd = "/mnt/scr1/lrknox/tools/bin/h5check $_";
    #   my $cmd = "./h5dump -H $_";
          # 1.6 has no h5stat and v1.8 h5stat returns 0 even if it fails to
          # open the file.
-         print "Program to test is $program with expected return value $RET.\n";
-         if ($program eq "h5dump -H"
-             || (-e "$dir/bin/$program"
-                 &&  ($program ne "h5copy"
-                      && ($dir eq "$STR19"
-                          || $dir eq "$NSTR19"
-                          || $dir eq "$STR18"
-                           || $dir eq "$NSTR18")
-                          && $RET == 0))) {
-
-         print "Program to test in 1st condition is $program.\n";
-            $cmd = "$dir/bin/$program $directory/$testfile";
-         } elsif ($program eq "h5copy" && -e "$dir/bin/$program") {
-            print "Testing h5copy.\n";
-            if (-e "$COPIES/$testfile") {
-               $cmd = `rm "$COPIES/$testfile"`;
-               my $output = `$cmd`;
-               sleep(10);
-            }
-            print "Running h5copy on $directory/$testfile\n";
-            $cmd = "$dir/bin/$program -i $directory/$testfile -o $COPIES/$testfile -s \"/\" -d \"/COPY\" ";
-         } else {
-            print "No $program found in $dir\n";
-            next;
-         }
-         if ($program eq "h5stat") {
-            open(STDERR,">stderr.txt");
-            $_ = `$cmd`;
-            if(-e "stderr.txt") {
-               $_ = $_.`cat stderr.txt`;
-            }
-         } else {
-            print "Running $cmd\n";
-            #$_ = `$cmd`;
-         }
-         my $output = `$cmd`;
-         #print $output;
-         if ($program eq "h5stat" && /Unable to traverse object/) {
-            $result = 1;
-         } else {
-            $result = $? >> 2;
-         }
-         evaluate_result ($RET, $result, $directory, $testfile, $dir, $program) ;
+         
       }
    }
    print "Done checking files with $dir.\n\n";
@@ -174,7 +197,7 @@ sub repack_file {
    #   my $cmd = "./h5dump -H $_";
       # 1.6 has no h5stat and v1.8 h5stat returns 0 even if it fails to
       # open the file.
-      print "Repacking $testfile with $dir/bin/h5repack\n";
+      print "Repacking $inputdir/$testfile to $outputdir/$testfile with $dir/bin/h5repack\n";
       my $cmd = "$dir/bin/h5repack $inputdir/$testfile $outputdir/$testfile";
       my $output = `$cmd`;
       evaluate_result ($RET, $?, $inputdir, $testfile, $dir, "h5repack") ;
@@ -227,7 +250,7 @@ sub dump_copies {
    if ($dir eq "$NSTR18" || $dir eq "$STR18" ) {
       $COPIES = "$COPYDIR/v180";
    } elsif ($dir eq "$NSTR19" || $dir eq "$STR19") {
-      $COPIES = "$COPYDIR/v180";
+      $COPIES = "$COPYDIR/v190";
    } else {
       return;
    }
@@ -299,9 +322,9 @@ print "Removing files from $REPACKED18\n";
 $cmd = `rm $REPACKED18/*`;
 print "Removing files from $REPACKED19\n";
 $cmd = `rm $REPACKED19/*`;
-#print "Removing files from $COPYDIR\n";
-#$cmd = `rm $COPYDIR/*`;
-
+print "Removing files from $COPYDIR\n";
+$cmd = `rm $COPYDIR/V180/*`;
+$cmd = `rm $COPYDIR/V190/*`;
 
 #print "Check corrupted files with strict checking - should not return 0.\n";
 foreach my $dir (@dirs) {
