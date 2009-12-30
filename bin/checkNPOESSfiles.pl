0b2e5c444a2b6daf196fb59f3c225e7b730a722e
Larry Knox <lrknox@hdfgroup.org>
2009-12-30 15:44:17 -0600

    Add new files snapshot-npoess-overrides, get_todays_npoess_files.pl, checkNPOESSfiles.pl for running backward/forward compatibility tests with NPOESS files.

---
+++ bin/checkNPOESSfiles.pl
@@ -0,0 +1,444 @@
+#!/usr/bin/perl
+# checkfiles.pl
+use warnings;
+use strict;
+use File::Path;
+
+mkpath(['copies/v18', 'copies/v19', 'repacked18', 'repacked19'], 1, 0755);
+
+#my $directory = shift;
+#my $SELECTED = shift;
+my $ENV_LD_LIBRARY_PATH = $ENV{LD_LIBRARY_PATH};
+my $OP_CONFIGURE = "";
+$OP_CONFIGURE = shift;
+my $HOST_NAME = `hostname | cut -f1 -d.`;  # no domain part`
+#my $HOST_NAME = substr $HOST_NAME_STR, 0, index ".", $HOST_NAME_STR;
+chomp($HOST_NAME);
+
+if ("$OP_CONFIGURE" ne "") {
+   $HOST_NAME = $HOST_NAME.$OP_CONFIGURE;
+}
+#my $PREFIX = "TestDir/$HOST_NAME";
+my $PREFIX = "";
+my $SELECTED = "/mnt/scr1/NPOESS/selected";
+my $CORRUPTED = "";
+#my $CORREPACKED18 = "$PREFIX/corrupted-repacked18/MLS";
+#my $CORREPACKED19 = "$PREFIX/corrupted-repacked19/MLS";
+#my $REPACKED18 = "$PREFIX/repacked18/";
+#my $REPACKED19 = "$PREFIX/repacked19/";
+#my $COPIES = "$PREFIX/copies";
+my $COPYDIR ="copies";
+my $CORREPACKED18 = "";
+my $CORREPACKED19 = "";
+my $REPACKED18 = "repacked18";
+my $REPACKED19 = "repacked19";
+#my $COPYDIR = "/mnt/scr1/NASA/copies";
+my $OOPS = "";
+my @ERR_MESSAGES = ("");
+my $HDF5_DIR = "/mnt/scr1/pre-release/hdf5/";
+
+my $STR16 = $HDF5_DIR."v16/".$HOST_NAME."-strict";
+my $NSTR16 = $HDF5_DIR."v16/".$HOST_NAME."-nostrict";
+my $STR18 = $HDF5_DIR."v18/".$HOST_NAME."-strict";
+my $NSTR18 = $HDF5_DIR."v18/".$HOST_NAME."-nostrict";
+my $STR19 = $HDF5_DIR."v19/".$HOST_NAME."-strict";
+my $NSTR19 = $HDF5_DIR."v19/".$HOST_NAME."-nostrict";
+#my $H5CHECK =  $HDF5_DIR."v18/compat/".$HOST_NAME."/bin/h5check";
+my @dirs = ($STR16, $NSTR16, $STR18, $NSTR18, $STR19, $NSTR19);
+my @binaries = ("h5ls", "h5dump", "h5stat", "h5copy");
+my $retval = 0;
+
+sub evaluate_result {
+   my $RET = shift;
+   my $result = shift;
+   my $directory = shift;
+   my $testfile = shift;
+   my $dir = shift;
+   my $program = shift;
+      print "Return value was $result\n";
+   if (($RET == 1 && $result == 0) || ($RET == 0 && $result != 0)) {
+      print "Return value was $result\n";
+      print "\nError:  $directory/$testfile does not match expectations with $dir/bin/$program!\n\n";
+      $OOPS = $testfile;
+      #return 1;
+   } else {
+      #print "$testfile is ok with $program.\n";
+      print ".";
+   }
+}
+
+my $check_commands = {  h5ls => sub { my $dir = shift;
+                                      my $directory = shift;
+                                      my $testfile = shift;
+                                      my $cmd = "$dir/bin/h5ls $directory/$testfile";
+                                      return $cmd;},
+                        h5dump => sub { my $dir = shift;
+                                      my $directory = shift;
+                                      my $testfile = shift;
+                                      my $cmd = "";  
+                                      if ($dir eq "$NSTR16" || $dir eq "$STR16" ) {
+                                         $cmd = "$dir/bin/h5dump -H $directory/$testfile";
+                                      } else {
+                                         $cmd = "$dir/bin/h5dump -R $directory/$testfile";
+                                      }          
+                                      return $cmd;},
+                        h5stat => sub { my $dir = shift;
+                                      my $directory = shift;
+                                      my $testfile = shift;
+                                      my $cmd = "$dir/bin/h5stat $directory/$testfile";
+                                      return $cmd;},
+                        h5copy => sub { my $dir = shift;
+                                      my $directory = shift;
+                                      my $testfile = shift;
+                                      my $COPIES = "";
+                                      if ($dir eq "$NSTR18" || $dir eq "$STR18" ) {
+                                         $COPIES = "$COPYDIR/v18";
+                                      } elsif ($dir eq "$NSTR19" || $dir eq "$STR19") {
+                                         $COPIES = "$COPYDIR/v19";
+                                      }
+                                      if (-e "$COPIES/$testfile") {
+                                          my $command = `rm "$COPIES/$testfile"`;
+                                          my $output = `$command`;
+                                          sleep(10);
+                                      }
+                                      print "Running h5copy on $directory/$testfile\n";
+                                      my $cmd = "$dir/bin/h5copy -i $directory/$testfile -o $COPIES/$testfile -s \"/\" -d \"/COPY\" ";}
+                     };
+
+#for future reference:  check_commands above is a hash of anonymous subroutines
+#(or maybe a reference to a hash of anonymous subroutines, or some such thing).
+#I created it to eliminate an impossible if statement and to avoid a 4 part
+#if/else series to create the correct command for each program.  It is invoked
+#below by $check_commands->{"$program"}->(). 
+
+
+sub check_dir {
+   my $dir = shift;
+   my $directory = shift;
+   my $RET = shift;
+   my $result;
+   my $cmd;
+
+   #system("setenv LD_LIBRARY_PATH $dir/lib");
+   #Add the current library to the previous LD_LIBRARY_PATH
+   $ENV{LD_LIBRARY_PATH} = "$ENV_LD_LIBRARY_PATH:$dir/lib";
+   system("echo \$LD_LIBRARY_PATH");
+
+   #opendir DH, "." or die "Couldn't open the current directory:  $!";
+   opendir DH, $directory or die "Couldn't open $directory:  $!";
+   while ($_ = readdir(DH)) {
+      next unless ($_ =~ /.*\.h5$/) || ($_ =~ /.*\.he5$/);
+      my $testfile = $_;
+      print "checking $testfile\n";
+      foreach my $program (@binaries) {
+         print "Program to test is $program with expected return value $RET.\n";
+         if (-e "$dir/bin/$program") {
+            $cmd = $check_commands->{"$program"}->($dir, $directory, $testfile);
+
+            # v1.8 h5stat returns 0 even if it fails to open the file.
+            if ($program eq "h5stat" && ($dir eq "$NSTR18" || $dir eq "$STR18"
+                                        || $dir eq "$NSTR19" || $dir eq "$STR19")) {
+               open(STDERR,">h5stat_stderr.txt");
+            }
+
+            # v1.8 h5ls returns 0 even if it fails to open the file.
+            if ($program eq "h5ls" && ($dir eq "$NSTR18" || $dir eq "$STR18"
+                                        || $dir eq "$NSTR19" || $dir eq "$STR19")) {
+               open(STDERR,">h5ls_stderr.txt");
+            }
+
+            $_ = `$cmd`;
+            $result = $? >> 2;
+
+            #check result if v1.8 h5stat and result == 0; remove err file.
+            if(-e "h5stat_stderr.txt") { 
+               if ($result == 0) {
+                  $_ = $_.`cat h5stat_stderr.txt`;
+                  if (/Unable to traverse object/) {
+                     $result = 1;
+                  }
+               }
+               `rm h5stat_stderr.txt`;      
+            }
+
+            #check result if v1.8 h5ls and result == 0; remove err file.
+            if(-e "h5ls_stderr.txt") { 
+               if ($result == 0) {
+                  $_ = $_.`cat h5ls_stderr.txt`;
+                  if (/unable to open file/) {
+                     $result = 1;
+                  }
+               }
+               `rm h5ls_stderr.txt`;      
+            }
+
+            #print $output;
+            evaluate_result ($RET, $result, $directory, $testfile, $dir, $program) ;
+         } else {
+            print "$program not found in $dir.\n";
+         }
+   #   my $cmd = "/mnt/scr1/lrknox/tools/bin/h5check $_";
+   #   my $cmd = "./h5dump -H $_";
+         # 1.6 has no h5stat and v1.8 h5stat returns 0 even if it fails to
+         # open the file.
+         
+      }
+   }
+   print "Done checking files with $dir.\n\n";
+}
+           
+sub countfiles {
+   my $directory = shift;
+   my $count = 0;
+
+   opendir DH, $directory or die "Couldn't open the current directory:  $!";
+   while ($_ = readdir(DH)) {
+      next unless ($_ =~ /.*\.h5$/) || ($_ =~ /.*\.he5$/);
+      ++$count;
+   }
+   return $count;
+}
+
+sub repack_file {
+   my $dir = shift;
+   my $inputdir = shift;
+   my $outputdir = shift;
+   my $RET = shift;
+   print "Entered repack_file with params $dir, $inputdir, $outputdir, $RET\n";
+   #print "Count files.\n";
+   my $filecount = countfiles($inputdir);
+   my $dayofmonth = `date +%e`;
+   my $filenum = $dayofmonth % $filecount;
+   $filecount = 0;
+   #Add the current library to the previous LD_LIBRARY_PATH
+   $ENV{LD_LIBRARY_PATH} = "$ENV_LD_LIBRARY_PATH:$dir/lib";
+   system("echo \$LD_LIBRARY_PATH");
+
+   #opendir DH, "." or die "Couldn't open the current directory:  $!";
+   opendir DH, $inputdir or die "Couldn't open the current directory:  $!";
+   while ($_ = readdir(DH)) {
+      next unless ($_ =~ /.*\.h5$/) || ($_ =~ /.*\.he5$/);
+      next unless ($filenum == $filecount++);
+      my $testfile = $_;
+   #   my $cmd = "/mnt/scr1/lrknox/tools/bin/h5check $_";
+   #   my $cmd = "./h5dump -H $_";
+      # 1.6 has no h5stat and v1.8 h5stat returns 0 even if it fails to
+      # open the file.
+      print "Repacking $inputdir/$testfile to $outputdir/$testfile with $dir/bin/h5repack\n";
+      my $cmd = "$dir/bin/h5repack $inputdir/$testfile $outputdir/$testfile";
+      my $output = `$cmd`;
+      evaluate_result ($RET, $?, $inputdir, $testfile, $dir, "h5repack") ;
+   }
+}
+
+sub check_diff {
+   my $dir = shift;
+   my $repackeddir = shift;
+   my $origdir = shift;
+   my $RET = shift;
+   #Add the current library to the previous LD_LIBRARY_PATH
+   $ENV{LD_LIBRARY_PATH} = "$ENV_LD_LIBRARY_PATH:$dir/lib";
+   system("echo \$LD_LIBRARY_PATH");
+
+   #opendir DH, "." or die "Couldn't open the current directory:  $!";
+   opendir DH, $repackeddir or die "Couldn't open the current directory:  $!";
+   while ($_ = readdir(DH)) {
+      next unless ($_ =~ /.*\.h5$/) || ($_ =~ /.*\.he5$/);
+      my $testfile = $_;
+      print "checking $testfile\n";
+
+      my $cmd = "$dir/bin/h5diff $repackeddir/$testfile $origdir/$testfile";
+      my $output = `$cmd`;
+   #  print $output;
+      evaluate_result ($RET, $?, $repackeddir, $testfile, $dir, "h5diff") ;
+   }
+}
+
+sub check_diffs {
+   my $dir = shift;
+   $_ = $dir;
+   #h5diff of selected vs repacked files should return zero
+   check_diff ($dir, $REPACKED18, $SELECTED, 0);
+   check_diff ($dir, $REPACKED19, $SELECTED, 0);
+}
+
+sub dump_copies {
+   my $dir = shift;
+   my $COPIES = "";
+   if ($dir eq "$NSTR18" || $dir eq "$STR18" ) {
+      $COPIES = "$COPYDIR/v18";
+   } elsif ($dir eq "$NSTR19" || $dir eq "$STR19") {
+      $COPIES = "$COPYDIR/v19";
+   } else {
+      return;
+   }
+   my $result;
+   #system("setenv LD_LIBRARY_PATH $dir/lib");
+   #Add the current library to the previous LD_LIBRARY_PATH
+   $ENV{LD_LIBRARY_PATH} = "$ENV_LD_LIBRARY_PATH:$dir/lib";
+   system("echo \$LD_LIBRARY_PATH");
+
+   print "Dumping copies in $COPIES with $dir/bin/h5dump.\n";
+   #opendir DH, "." or die "Couldn't open the current COPIES:  $!";
+   opendir DH, $COPIES or die "Couldn't open the current COPIES:  $!";
+   while ($_ = readdir(DH)) {
+      my $cmd = "";
+      next unless ($_ =~ /.*\.h5$/) || ($_ =~ /.*\.he5$/);
+      my $testfile = $_;
+      if ($dir eq "$NSTR16" || $dir eq "$STR16" ) {
+         $cmd = "$dir/bin/h5dump -H $COPIES/$testfile";
+      } else {
+         $cmd = "$dir/bin/h5dump -R $COPIES/$testfile";
+      }
+      open(STDERR,">stderr.txt");
+      $_ = `$cmd`;
+      $result = $?;
+      my $output;
+      if(-e "stderr.txt") {
+         $output = $_.`cat stderr.txt`;
+      } else {
+         $output = `$cmd`;
+      }
+      print "Return value was $result\n";
+      #print $output;
+      if ($result != 0) {
+         print "Copied file $testfile cannot be dumped by $dir/bin/h5dump.\n";
+         $OOPS = $testfile;
+      } else {
+         print "Copied file $testfile successfully dumped by $dir/bin/h5dump.\n";
+      }
+   }
+}
+
+sub run_h5check {
+   my $dir = shift;
+   my $directory = shift;
+   my $RET = shift;
+   my $result;
+   #Add the current library to the previous LD_LIBRARY_PATH
+   #$ENV{LD_LIBRARY_PATH} = "$ENV_LD_LIBRARY_PATH:$dir/lib";
+   #system("echo \$LD_LIBRARY_PATH");
+
+   #opendir DH, "." or die "Couldn't open the current directory:  $!";
+   opendir DH, $directory or die "Couldn't open the current directory:  $!";
+   while ($_ = readdir(DH)) {
+      next unless ($_ =~ /.*\.h5$/) || ($_ =~ /.*\.he5$/);
+      my $testfile = $_;
+      print "checking $testfile\n";
+
+      #my $cmd = "$H5CHECK $directory/$testfile";
+      my $cmd = "$dir/bin/h5check $directory/$testfile";
+      my $output = `$cmd`;
+      #$result = $?;
+
+
+      #print "$cmd returned $result"."\nOutput was: $output"."\n";
+      evaluate_result ($RET, $?, $directory, $testfile, $dir, "h5check") ;
+   }
+}
+
+print "Removing files from $REPACKED18\n";
+my $cmd = `rm $REPACKED18/*`;
+print "Removing files from $REPACKED19\n";
+$cmd = `rm $REPACKED19/*`;
+print "Removing files from $COPYDIR\n";
+$cmd = `rm $COPYDIR/v18/*`;
+$cmd = `rm $COPYDIR/v19/*`;
+
+print "Repack the selected files with strict checking - should return 0.\n";
+foreach my $dir (@dirs) {
+   $_ = $dir;
+   print "Consider $dir.\n";
+   next if (/nostrict/);
+   if ($dir eq "$STR18") {
+      print "Repack with $dir?\n";
+      repack_file ($dir, $SELECTED, $REPACKED18, 0);
+   }
+
+   if ($dir eq "$STR19") {
+      print "Repack with $dir?\n";
+      repack_file ($dir, $SELECTED, $REPACKED19, 0);
+   }
+}
+if ($OOPS ne "") {
+   print "Selected files were not all correctly repacked:\n";
+   push @ERR_MESSAGES, "Selected files were not all correctly repacked:\n";
+   print $OOPS;
+   $retval += 1;
+}
+
+
+
+print "Check selected and repacked files with strict and non-strict checking - should return 0 if ok.\n";
+foreach my $dir (@dirs) {
+   check_dir ($dir, $SELECTED, 0);
+}
+foreach my $dir (@dirs) {
+   check_dir ($dir, $REPACKED18, 0);
+}
+foreach my $dir (@dirs) {
+   check_dir ($dir, $REPACKED19, 0);
+}
+
+if ($OOPS ne "") {
+   print "Corrupt file found:\n";
+   push @ERR_MESSAGES, "Corrupt file found:\n";
+   print $OOPS;
+   $retval += 1;
+   $OOPS = "";
+}
+
+foreach my $dir (@dirs) {
+   print "Check all files with $dir/bin/h5check.\n";
+
+   run_h5check($dir, $SELECTED, 0);
+   run_h5check($dir, $REPACKED18, 0);
+   run_h5check($dir, $REPACKED19, 0);
+   if ($OOPS ne "") {
+      print "h5check found file with unexpected errors.\n";
+      push @ERR_MESSAGES, "h5check found file with unexpected errors.\n";
+      print $OOPS;
+      $retval += 1;
+      $OOPS = "";
+   } else {
+      print "All files ok with $dir/bin/h5check.\n";
+   }
+}
+
+# check_diffs runs h5diff on repacked files
+#disable while h5repack is not working.
+push @ERR_MESSAGES, "check_diffs is disabled in line 408 until the h5repack bug that skips attributes for datasets with references is fixed.\n";   
+foreach my $dir (@dirs) {
+   #check_diffs ($dir)
+}
+if ($OOPS ne "") {
+   print "h5diff can't open file or repacked file does no match original file.\n";
+   push @ERR_MESSAGES, "h5diff can't open file or repacked file does no match original file.\n";
+   print $OOPS;
+   $retval += 1;
+   $OOPS = "";
+}
+
+#dump_copies attempts to dump files copied with h5copy
+foreach my $dir (@dirs) {
+   $_ = $dir;
+   next if (/nostrict/);
+   dump_copies ($dir)
+}
+if ($OOPS ne "") {
+   print "File produced by h5copy can't be dumped with h5dump.\n";
+   push @ERR_MESSAGES, "File produced by h5copy can't be dumped with h5dump.\n";
+   print $OOPS;
+   $retval += 1;
+   $OOPS = "";
+}
+
+if ($retval == 0) {
+   print "checkfiles.pl works properly and no problem files found in $SELECTED.\n";
+   print @ERR_MESSAGES;
+} else {
+   print @ERR_MESSAGES;
+}
+exit $retval;
+
+
---
+++ bin/get_todays_npoess_files.pl
@@ -0,0 +1,298 @@
+#!/usr/bin/perl -w
+use strict;
+
+use Net::FTP;
+my @ls;
+my $this;
+my $sname;
+my $sdir;
+my @dirstack;
+my $jpdir;
+my $tsize;
+my $count;
+my $success;
+my @time = localtime();
+my $day = $time[7] + 1;
+my $dom = $time[3];
+my $mon = $time[4] + 1;
+my $mddate = "";
+my $ymddate = "";
+
+
+sub select_server_and_directory {
+   my $tes = shift;
+   my $doy = shift;
+   my $serverfile;
+   my $directorynum;
+# Only one type of directory for NPOESS at present
+#   if ($tes ne "true") {
+#      $tes = "";
+#      $serverfile = "/home/lrknox/perlscripts/EOSdirs.list";
+#      $directorynum = $doy % 76;
+#   } else {
+      $serverfile = "/home/lrknox/perlscripts/NPOESSdirs.list";
+      $directorynum = $doy % 26;
+#   }
+   my $lineno = 0;
+   print "Opening $serverfile.\n";
+   open(INFILE, $serverfile);
+   while (<INFILE>) {
+      #++$lineno;
+      next unless $lineno++ == $directorynum;
+      ($sname, $sdir) = split /,/;
+      print "Selected $sname & $sdir\n";
+      chomp($sname);
+      chomp($sdir)
+   }
+   close(INFILE);
+
+}
+
+sub push_nontes_dirs {
+   my $day = shift;
+   foreach (@{$jpdir}) {
+      @ls = split;
+   
+      # skip hidden, parent and self!
+      next if ( not defined($ls[8]) or $ls[8] =~ /^\./);
+             # only interested in directories named by 4 digits or this day of the year.
+      print "Considering $ls[8].\n"; 
+      next unless (defined($ls[8]) and ($ls[8] =~ /\d{4}/ or $ls[8] =~ /\/$day\//)); 
+      print "Push $ls[8] if a directory.\n";
+      if (defined($ls[0]) and $ls[0] =~ /^d/) {
+         push @dirstack,$sdir."/".$ls[8];
+         print $ls[8],"\n";
+         next;
+      }
+      # this block would list any files in the root directory - so far there aren't any.
+      # if any turn up we need to think about what to do with them.
+      if ($ls[0] =~ /^-/ and $ls[8] =~ /h.5$/)
+      {
+         print $ls[8],"\n";
+         $tsize += $ls[4];
+         $count ++;
+      }
+   }
+}
+
+# the tes server has its directories organized and named by yyyy.mm.dd.  That
+# could be handled in the subroutine above, but the server returns lines with
+# one less field than the other servers, hence the separate subroutine.
+sub push_tes_dirs {
+   my $day = shift;
+   foreach (@{$jpdir}) {
+      @ls = split;
+   
+      # skip hidden, parent and self!
+      next if ( not defined($ls[7]) or $ls[7] =~ /^\./);
+             # only interested in directories named by 4 digits or this day of the year.
+      print "Considering $ls[7].\n"; 
+      next unless (defined($ls[7]) and ($ls[7] =~ /\d{4}/ or $ls[7] =~ /\/$day\//)); 
+      print "Push $ls[7] if a directory.\n";
+      if (defined($ls[0]) and $ls[0] =~ /^d/) {
+         push @dirstack,$sdir."/".$ls[7];
+         print $ls[7],"\n";
+         next;
+      }
+      # this block would list any files in the root directory - so far there aren't any.
+      # if any turn up we need to think about what to do with them.
+      if ($ls[0] =~ /^-/ and ($ls[7] =~ /h.5$/))
+      {
+         print $ls[7],"\n";
+         $tsize += $ls[4];
+         $count ++;
+      }
+   }
+}
+
+#using this one at least initially for NPOESS files
+sub download_files {
+   my $day = shift;
+   my $filecount = 0;
+   foreach (@{$jpdir}) {
+      @ls = split;
+      #print "\nPrinting all 8 fields:\n";
+      #foreach (@ls) {
+      #   print $_, "\n";
+      #}
+      # skip hidden, parent and self!
+      next if ($ls[8] and $ls[8] =~ /^\./);
+      if ($ls[0] =~ /^d/) {
+         # only interested in directories named by 4 digits or this day of the year.
+         print "Handling directory $ls[8].\n";
+         next unless ($ls[8] =~ /\d{4}/ or $ls[8] =~ /$mon$/ or $ls[8] =~ /$day/); 
+         print "Push $this/$ls[8] onto directory stack.\n";
+         push @dirstack,$this."/".$ls[8];
+         next;
+      }
+   
+      next unless ($ls[8] =~ /\.he?5$/); 
+      print "Match:  ".$this."/".$ls[8],"\n";
+      $success = $Net::FTP::ftpobj->get($ls[8], "/mnt/scr1/NPOESS/selected/$ls[8]");
+      if ($success eq "") {
+         print $Net::FTP::ftp->message() . "\n";
+      }      	
+      $tsize += $ls[4];
+      $filecount ++;
+      last if ($filecount >5); 
+   }
+}
+
+sub download_nontes_files {
+   my $day = shift;
+   foreach (@{$jpdir}) {
+      @ls = split;
+      # skip hidden, parent and self!
+      next if ($ls[8] and $ls[8] =~ /^\./);
+      if ($ls[0] =~ /^d/) {
+         # only interested in directories named by 4 digits or this day of the year.
+         print "Handling directory $ls[8].\n";
+         next unless ($ls[8] =~ /\d{4}/ or $ls[8] =~ /$mon$/ or $ls[8] =~ /$day/); 
+         print "Push $this/$ls[8] onto directory stack.\n";
+         push @dirstack,$this."/".$ls[8];
+         next;
+      }
+   
+      if ($ls[0] =~ /^-/ and ($ls[8] =~ /h.5$/ or $ls[8] =~ /h5$/)  and ($ls[8] =~ /\/$day\// or $ls[8] =~ /\d{4}$mddate/ or $ls[8] =~ /\d{4}d$day/ or $ls[8] =~ /$ymddate/))
+      {
+         print "Match:  ".$this."/".$ls[8],"\n";
+         $success = $Net::FTP::ftpobj->get($ls[8], "/mnt/scr1/NASA/selected/$ls[8]");
+         if ($success eq "") {
+            print $Net::FTP::ftp->message() . "\n";
+         }      	
+         $tsize += $ls[4];
+         $count ++;
+      }
+   }
+}
+
+sub download_tes_files {
+   my $day = shift;
+   foreach (@{$jpdir}) {
+      @ls = split;
+      # skip hidden, parent and self!
+      next if ($ls[7] =~ /^\./);
+      # TES directories shouldn't really have any interesting subdirectories,
+      # but it shouldn't hurt to check.
+      if ($ls[0] =~ /^d/) {
+         # only interested in directories named by 4 digits or this day of the year.
+         print "Handling directory $ls[7].\n";
+         next unless ($ls[7] =~ /\d{4}/ or $ls[7] =~ /$mon$/ or $ls[7] =~ /$day/); 
+         print "Push $this/$ls[7] onto directory stack.\n";
+         push @dirstack,$this."/".$ls[7];
+         next;
+      }
+      # The TES files have year, month, and day in the directory name, so get
+      # any h5 or he5 files in the directory
+      if ($ls[0] =~ /^-/ and ($ls[7] =~ /h.5$/ or $ls[7] =~ /h5$/))
+      {
+         print "Match:  ".$this."/".$ls[7],"\n";
+         $success = $Net::FTP::ftpobj->get($ls[7], "/mnt/scr1/NASA/selected/$ls[7]");
+         if ($success eq "") {
+            print $Net::FTP::ftp->message() . "\n";
+         }      	
+         $tsize += $ls[4];
+         $count ++;
+      }
+   }
+}
+
+$success = 1;
+
+if ($day < 10) {
+   $day = "00".$day;
+} elsif ($day < 100) {
+   $day = "0".$day;
+}
+if ($mon < 10) {
+   $mddate = "m0".$mon;
+   $ymddate = "0".$mon;
+} else {
+   $mddate = "m".$mon;
+   $ymddate = $mon;
+}
+if ($dom < 10) {
+   $mddate = $mddate."0".$dom;
+   $ymddate = $ymddate."0".$dom;
+} else {
+   $mddate = $mddate.$dom;
+   $ymddate = "$ymddate.$dom";
+} 
+print " Today is day $day of the year.  The month and day is $mddate.\n";
+# searches for files
+# matching a certain
+# pattern within or below a
+# named directory on an FTP
+# server
+my $tes = "";
+my $servers = 0;
+# ONLY one server for NPOESS at this point
+#while ($servers < 2) {
+   print "Search FTP site!\n";
+   select_server_and_directory($tes, $day);
+   # timeout set to 20 seconds
+   # in case a system isn't
+   # reachable, even though
+   # it's known!
+   
+   $Net::FTP::ftpobj = Net::FTP -> new ($sname,Timeout=>20) or
+           die "Cannot access $sname via FTP\n";
+   print "Connecting to $sname for FTP.\n";
+   # Ensure that password is not echoed!
+   
+   my $loginname = "anonymous";
+   
+   $Net::FTP::pwd = "hdftest\@hdfgroup.org";
+   print "Logging in as $loginname with password $Net::FTP::pwd.\n";
+   
+   $Net::FTP::ftpobj -> login($loginname,$Net::FTP::pwd) or
+           die "Invalid user name and/or password\n";
+   print "Logged in as anonymous.\n";
+   
+   # Check that given root really exists!
+   
+   print "Directory to search from:  $sdir.\n";
+   
+   $Net::FTP::ftpobj -> cwd ("$sdir") or
+           die "Cannot access this directory\n";
+   
+   $Net::FTP::ftpobj -> binary or
+           die "Cannot set mode to binary: $!\n";
+   
+   # @dirstack is a queue of directories to be
+   # visited
+   print "List files in $sdir.\n";
+   
+   $jpdir = $Net::FTP::ftpobj -> dir;
+# No subdirs for NPOESS at present
+   push @dirstack,$sdir;
+#   if ($tes ne "true") {
+#      push_nontes_dirs($day);
+#   } else {
+#      push_tes_dirs($day);
+#   }
+   
+   while ($this = shift @dirstack)
+   {
+      $Net::FTP::ftpobj -> cwd ("$this") or warn "Couldn't change directory.";
+      my $currentdir = $Net::FTP::ftpobj -> pwd();
+      #print "Current directory:  $currentdir\tThis:  $this\n";
+      next unless $currentdir eq $this;
+      print "Changed directory to $this.\n";
+      $jpdir = $Net::FTP::ftpobj -> dir;
+      if ($tes ne "true") {
+         download_files($day);
+         #download_nontes_files($day);
+      } else {
+         download_tes_files($day);
+      }
+   }
+   
+   $Net::FTP::ftpobj -> quit;
+   if (defined($count) and defined($tsize)) {
+      print "$count files, total size $tsize bytes\n";
+   }
+# No tes for NPOESS
+#   $tes = "true";
+   $servers++;
+#}
