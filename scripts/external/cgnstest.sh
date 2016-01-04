7d59066e61bf53751eb6cfd13824698e07f0fe90
Scot Breitenfeld <brtnfld@hdfgroup.org>
2016-01-04 16:24:46 -0600

    changed script to accept two command line arguments: testing id and compiler version, cmake is disabled

--- scripts/external/cgnstest.sh
+++ scripts/external/cgnstest.sh
@@ -12,50 +12,194 @@ UNAME="unknown"
 if [ -x /usr/bin/uname ]
 then
   UNAME=`/usr/bin/uname -n`
+  PROC=`/usr/bin/uname -p`
 fi
 if [ -x /usr/local/bin/uname ]
 then
   UNAME=`/usr/local/bin/uname -n`
+  PROC=`/usr/local/bin/uname -p`
 fi
 if [ -x /bin/uname ]
 then
   UNAME=`/bin/uname -n`
+  PROC=`/bin/uname -p`
 fi
 
-# Read command line for the test to run
+if [[ $UNAME ==  "osx1010test.ad.hdfgroup.org" ]]; then
+    UNAME="osx1010test"
+fi
+
+if [[ $UNAME == "moohan.ad.hdfgroup.org" ]]; then
+    UNAME="moohan"
+fi
+
+# READ COMMAND LINE FOR THE TEST TO RUN
 
 TEST_NO=$1
+shift
+TEST_COMPILER=$1
 shift
 
 make_bin="make"
 
-ENABLE_64BIT=""
-ENABLE_PARALLEL=""
-ENABLE_SZIP=""
 HDF_DIR="0"
+
+# COMPILER TESTS: LINKED TO THE CORRESPONDING COMPILED HDF5 LIBRARY
+
+export FLIBS="-Wl,--no-as-needed -ldl"
+export LIBS="-Wl,--no-as-needed -ldl"
+CMAKE_EXE_LINKER_FLAGS="-D CMAKE_EXE_LINKER_FLAGS:STRING=\"-Wl,--no-as-needed -ldl\""
+DASH="-"
+
+if [[ $TEST_COMPILER == "" ]]; then # System default compiler, exclude dash in the path name
+    export CC="gcc"
+    export FC="gfortran"
+    DASH=""
+elif [[ $TEST_COMPILER == "gcc52" ]]; then
+    export CC="/usr/hdf/bin/gcc52/gcc"
+    export FC="/usr/hdf/bin/gcc52/gfortran"
+elif [[ $TEST_COMPILER == "gcc49" ]]; then
+    export CC="/usr/hdf/bin/gcc49/gcc"
+    export FC="/usr/hdf/bin/gcc49/gfortran"
+elif [[ $TEST_COMPILER == "gcc48" ]]; then
+    export CC="/usr/hdf/bin/gcc48/gcc"
+    export FC="/usr/hdf/bin/gcc48/gfortran"
+elif [[ $TEST_COMPILER == "intel" ]]; then
+    export CC="icc"
+    export FC="ifort"
+    export FLIBS=""
+    export LIBS=""
+    CMAKE_EXE_LINKER_FLAGS=""
+elif [[ $TEST_COMPILER == "pgi" ]]; then
+    export CC="pgcc"
+    export FC="pgf90"
+elif [[ $TEST_COMPILER == "pp" ]]; then
+    MPI="/mnt/hdf/packages/mpich/3.1.4_gnu4.9.2/$PROC/bin"
+    export CC="$MPI/mpicc"
+    export FC="$MPI/mpif90"
+    export FFLAGS=""
+elif [[ $TEST_COMPILER == "xl" ]]; then
+    export CC="/opt/xl/xlf15.1_xlc13.1/xlc"
+    export FC="/opt/xl/xlf15.1_xlc13.1/xlf"
+    export FLIBS=""
+    export LIBS=""
+    CMAKE_EXE_LINKER_FLAGS=""
+elif [[ $TEST_COMPILER == "emu64" ]]; then
+    export CC="cc"
+    export FC="f90"
+    export FCFLAGS="-m64"
+    export CFLAGS="-m64"
+    export FLIBS=""
+    export LIBS=""
+    CMAKE_EXE_LINKER_FLAGS=""
+else
+    echo " *** TESTING SCRIPT ERROR ***"
+    echo "   - Unknown compiler specified: $TEST_COMPILER"
+    exit 1
+fi
+
+# See Document CGNS_TestingSpecs.docx in the CGNS source git repository
+
+#AUTOCONF
+WITH_FORTRAN=""
+ENABLE_PARALLEL=""
+WITH_HDF5=""
+ENABLE_64BIT=""
+ENABLE_LEGACY=""
+ENABLE_SCOPE=""
+ENABLE_LFS=""
+ENABLE_DEBUG=""
+ENABLE_SZIP=""
+#CMAKE
+CGNS_ENABLE_64BIT="-D CGNS_ENABLE_64BIT:BOOL=OFF"
+CGNS_ENABLE_FORTRAN="-D CGNS_ENABLE_FORTRAN:BOOL=OFF"
+CGNS_ENABLE_HDF5="-D CGNS_ENABLE_HDF5:BOOL=OFF"
+CGNS_ENABLE_LFS="-D CGNS_ENABLE_LFS:BOOL=OFF"
+CGNS_ENABLE_PARALLEL="-D CGNS_ENABLE_PARALLEL:BOOL=OFF"
+
+HDF_DIR="/mnt/scr1/pre-release/hdf5/vdev/$UNAME$DASH$TEST_COMPILER" 
+TEST_SZIP=`grep -iq "szip" $HDF_DIR/bin/h5*cc;echo $?`
+if [[ $TEST_SZIP == 0 ]]; then
+    SZIP=`cat $HDF_DIR/bin/h5*cc | grep "H5BLD_LDFLAGS=" | sed -e 's/.*H5BLD_LDFLAGS=" -L\(.*\) ".*/\1/'`
+    ENABLE_SZIP="--with-szip=$SZIP/libsz.a"
+fi
+
+if [[ $TEST_NO == 1 ]]; then
+    WITH_FORTRAN="--with-fortran=yes"
+    CGNS_ENABLE_FORTRAN="-D CGNS_ENABLE_FORTRAN:BOOL=ON"
+    ENABLE_64BIT="--enable-64bit"
+    CGNS_ENABLE_64BIT="-D CGNS_ENABLE_64BIT:BOOL=ON"
+    ENABLE_LEGACY="--enable-legacy"
+    ENABLE_LFS="--enable-lfs"
+    CGNS_ENABLE_LFS="-D CGNS_ENABLE_LFS:BOOL=ON"
+    ENABLE_DEBUG="--enable-debug"
+elif [[ $TEST_NO == 2 ]]; then
+    WITH_FORTRAN="--with-fortran=yes"
+    CGNS_ENABLE_FORTRAN="-D CGNS_ENABLE_FORTRAN:BOOL=ON"
+    ENABLE_DEBUG="--enable-debug"
+elif [[ $TEST_NO == 3 ]]; then
+    WITH_FORTRAN="--with-fortran=no"
+    CGNS_ENABLE_FORTRAN="-D CGNS_ENABLE_FORTRAN:BOOL=ON"
+    WITH_HDF5="$ENABLE_SZIP --with-zlib --with-hdf5=$HDF_DIR"
+    ENABLE_64BIT="--enable-64bit"
+    CGNS_ENABLE_64BIT="-D CGNS_ENABLE_64BIT:BOOL=ON"
+    ENABLE_LEGACY="--enable-legacy"
+    ENABLE_LFS="--enable-lfs"
+    CGNS_ENABLE_LFS="-D CGNS_ENABLE_LFS:BOOL=ON"
+    ENABLE_DEBUG="--enable-debug"
+    CGNS_ENABLE_HDF5="-D CGNS_ENABLE_HDF5:BOOL=ON -D HDF5_LIBRARY:STRING=$HDF_DIR/lib/libhdf5.a -D HDF5_INCLUDE_PATH:PATH=$HDF_DIR/include -D HDF5_NEED_ZLIB:BOOL=ON -D HDF5_NEED_SZIP:BOOL=$ENABLE_SZIP"
+elif [[ $TEST_NO == 4 ]]; then
+    WITH_FORTRAN="--with-fortran=yes"
+    CGNS_ENABLE_FORTRAN="-D CGNS_ENABLE_FORTRAN:BOOL=ON"
+    WITH_HDF5="$ENABLE_SZIP --with-zlib --with-hdf5=$HDF_DIR"
+    SZIP=`cat $HDF_DIR/bin/h5*cc | grep "H5BLD_LDFLAGS=" | sed -e 's/.*H5BLD_LDFLAGS=" -L\(.*\) ".*/\1/'`
+    WITH_HDF5="--with-szip=$SZIP/libsz.a --with-zlib --with-hdf5=$HDF_DIR"
+    ENABLE_LFS="--enable-lfs"
+    CGNS_ENABLE_LFS="-D CGNS_ENABLE_LFS:BOOL=ON"
+    CGNS_ENABLE_HDF5="-D CGNS_ENABLE_HDF5:BOOL=ON -D HDF5_LIBRARY:STRING=$HDF_DIR/lib/libhdf5.a -D HDF5_INCLUDE_PATH:PATH=$HDF_DIR/include -D HDF5_NEED_ZLIB:BOOL=ON -D HDF5_NEED_SZIP:BOOL=$ENABLE_SZIP"
+elif [[ $TEST_NO == 5 ]]; then
+    WITH_FORTRAN="--with-fortran=yes"
+    CGNS_ENABLE_FORTRAN="-D CGNS_ENABLE_FORTRAN:BOOL=ON"
+    WITH_HDF5="$ENABLE_SZIP --with-zlib --with-hdf5=$HDF_DIR"
+    ENABLE_PARALLEL="--with-mpi=$MPI --enable-parallel"
+    ENABLE_64BIT="--enable-64bit"
+    CGNS_ENABLE_64BIT="-D CGNS_ENABLE_64BIT:BOOL=ON"
+    ENABLE_LFS="--enable-lfs"
+    CGNS_ENABLE_LFS="-D CGNS_ENABLE_LFS:BOOL=ON"
+    ENABLE_DEBUG="--enable-debug"
+    CGNS_ENABLE_HDF5="-D CGNS_ENABLE_HDF5:BOOL=ON -D HDF5_LIBRARY:STRING=$HDF_DIR/lib/libhdf5.a -D HDF5_INCLUDE_PATH:PATH=$HDF_DIR/include -D HDF5_NEED_ZLIB:BOOL=ON -D HDF5_NEED_SZIP:BOOL=$ENABLE_SZIP"
+    CGNS_ENABLE_PARALLEL="-D CGNS_ENABLE_PARALLEL:BOOL=ON -D HDF5_NEED_MPI:BOOL=ON"
+elif [[ $TEST_NO == 6 ]]; then
+    WITH_FORTRAN="--with-fortran=yes"
+    CGNS_ENABLE_FORTRAN="-D CGNS_ENABLE_FORTRAN:BOOL=ON"
+    ENABLE_PARALLEL="--with-mpi=$MPI --enable-parallel"
+    WITH_HDF5="$ENABLE_SZIP --with-zlib --with-hdf5=$HDF_DIR"
+    ENABLE_LEGACY="--enable-legacy"
+    CGNS_ENABLE_HDF5="-D CGNS_ENABLE_HDF5:BOOL=ON -D HDF5_LIBRARY:STRING=$HDF_DIR/lib/libhdf5.a -D HDF5_INCLUDE_PATH:PATH=$HDF_DIR/include -D HDF5_NEED_ZLIB:BOOL=ON -D HDF5_NEED_SZIP:BOOL=$ENABLE_SZIP"
+    CGNS_ENABLE_PARALLEL="-D CGNS_ENABLE_PARALLEL:BOOL=ON -D HDF5_NEED_MPI:BOOL=ON"
+else
+    echo " *** TESTING SCRIPT ERROR ***"
+    echo "   - Unknown test id specified: $TEST_NO"
+    exit 2
+fi
+
 do_test=1 # default is to perform the tests
 
-if [[ $UNAME ==  "jam" ]]; then
-#jam gnu 5.2, serial
-    if [[ $TEST_NO == 1 ]]; then
-	export CC="/usr/hdf/bin/gcc52/gcc"
-	export FC="/usr/hdf/bin/gcc52/gfortran"
-	export FLIBS="-Wl,--no-as-needed -ldl"
-	export LIBS="-Wl,--no-as-needed -ldl"
-	ENABLE_64BIT="--enable-64bit"
+#if [[ $UNAME ==  "jam" ]]; then
+
+#    if [[ $TEST_NO == 1 ]]; then
+#	export FLIBS="-Wl,--no-as-needed -ldl"
+#	export LIBS="-Wl,--no-as-needed -ldl"
+#	ENABLE_64BIT="--enable-64bit"
 #jam intel, serial
-    elif [[ $TEST_NO == 2 ]]; then
-	export CC="icc"
-	export FC="ifort"
-	export FLIBS=""
-	export LIBS=""
+#    elif [[ $TEST_NO == 2 ]]; then
+#	export FLIBS=""
+#	export LIBS=""
       #ENABLE_64BIT="--enable-64bit"
 #jam pgi, serial
-    elif [[ $TEST_NO == 3 ]]; then
-	export CC="pgcc"
-	export FC="pgf90"
-	export FLIBS="-Wl,--no-as-needed -ldl"
-	export LIBS="-Wl,--no-as-needed -ldl"
+#    elif [[ $TEST_NO == 3 ]]; then
+#	export FLIBS="-Wl,--no-as-needed -ldl"
+#	export LIBS="-Wl,--no-as-needed -ldl"
       #ENABLE_64BIT="--enable-64bit"
 #jam gnu 5.2, serial, -fdefault-real-8 -fdefault-integer-8
 #  elif [[ $TEST_NO == 4 ]]; then
@@ -65,19 +209,15 @@ if [[ $UNAME ==  "jam" ]]; then
 #    export FLIBS="-Wl,--no-as-needed -ldl"
 #    export LIBS="-Wl,--no-as-needed -ldl"
 #    ENABLE_64BIT="--enable-64bit"
-    elif [[ $TEST_NO == 4 ]]; then
-	MPI="/mnt/hdf/packages/mpich/3.1.4_gnu4.9.2/i686/bin"
-	export CC="$MPI/mpicc"
-	export FC="$MPI/mpif90"
-	export FFLAGS=""
-	export FLIBS="-Wl,--no-as-needed -ldl"
-	export LIBS="-Wl,--no-as-needed -ldl"
-	ENABLE_64BIT="--enable-64bit"
-	ENABLE_PARALLEL="--enable-parallel"
-	WITH_MPI="--with-mpi=$MPI"
-    else
-	do_test=0
-    fi
+#    elif [[ $TEST_NO == 4 ]]; then
+#	export FLIBS="-Wl,--no-as-needed -ldl"
+#	export LIBS="-Wl,--no-as-needed -ldl"
+#	ENABLE_64BIT="--enable-64bit"
+#	ENABLE_PARALLEL="--enable-parallel"
+#	WITH_MPI="--with-mpi=$MPI"
+#    else
+#	do_test=0
+#    fi
 ###########################################
 #   _   _   _   _   _   _   _   _  
 #  / \ / \ / \ / \ / \ / \ / \ / \ 
@@ -85,68 +225,6 @@ if [[ $UNAME ==  "jam" ]]; then
 #  \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ 
 #
 ###########################################
-elif [[ $UNAME == "platypus" ]]; then
-#jam gnu , serial
-    if [[ $TEST_NO == 1 ]]; then
-     # export CC="/usr/hdf/bin/gcc52/gcc"
-    #  export FC="/usr/hdf/bin/gcc52/gfortran"
-	export CC="gcc"
-	export FC="gfortran"
-	export FLIBS="-Wl,--no-as-needed -ldl"
-	export LIBS="-Wl,--no-as-needed -ldl"
-	HDF_DIR="/mnt/scr1/pre-release/hdf5/vdev/platypus" 
-	SZIP=`cat $HDF_DIR/bin/h5cc | grep "H5BLD_LDFLAGS=" | sed -e 's/.*H5BLD_LDFLAGS=" -L\(.*\) ".*/\1/'`
-	ENABLE_SZIP="--with-szip=$SZIP/libsz.a"
-      #ENABLE_64BIT="--enable-64bit"
-#jam intel, serial
-    elif [[ $TEST_NO == 2 ]]; then
-	export CC="icc"
-	export FC="ifort"
-	export FLIBS=""
-	export LIBS=""
-      #ENABLE_64BIT="--enable-64bit"
-	HDF_DIR="/mnt/scr1/pre-release/hdf5/vdev/platypus-intel" 
-	SZIP=`cat $HDF_DIR/bin/h5cc | grep "H5BLD_LDFLAGS=" | sed -e 's/.*H5BLD_LDFLAGS=" -L\(.*\) ".*/\1/'`
-	ENABLE_SZIP="--with-szip=$SZIP/libsz.a"
-#jam pgi, serial
-    elif [[ $TEST_NO == 3 ]]; then
-	export CC="pgcc"
-	export FC="pgf90"
-	export FLIBS="-Wl,--no-as-needed -ldl"
-	export LIBS="-Wl,--no-as-needed -ldl"
-	HDF_DIR="/mnt/scr1/pre-release/hdf5/vdev/platypus-pgi"
-      #ENABLE_64BIT="--enable-64bit"
-#jam gnu 5.2, serial, -fdefault-real-8 -fdefault-integer-8
-#  elif [[ $TEST_NO == 4 ]]; then
-#    export CC="/usr/hdf/bin/gcc52/gcc"
-#    export FC="/usr/hdf/bin/gcc52/gfortran"
-#    export FFLAGS="-fdefault-real-8 -fdefault-integer-8"
-#    export FLIBS="-Wl,--no-as-needed -ldl"
-#    export LIBS="-Wl,--no-as-needed -ldl"
-#    ENABLE_64BIT="--enable-64bit"
-    elif [[ $TEST_NO == 4 ]]; then
-	MPI="/mnt/hdf/packages/mpich/3.1.4_gnu4.9.2/x86_64/bin"
-	export CC="$MPI/mpicc"
-	export FC="$MPI/mpif90"
-	export FFLAGS=""
-	export FLIBS="-Wl,--no-as-needed -ldl"
-	export LIBS="-Wl,--no-as-needed -ldl"
-	ENABLE_64BIT="--enable-64bit"
-	ENABLE_PARALLEL="--enable-parallel"
-	WITH_MPI="--with-mpi=$MPI"
-    elif [[ $TEST_NO == 5 ]]; then
-	MPI="/mnt/hdf/packages/mpich/3.1.4_intel15.0.3/x86_64/bin"
-	export CC="$MPI/mpicc"
-	export FC="$MPI/mpif90"
-	export FFLAGS=""
-	export FLIBS="-Wl,--no-as-needed -ldl"
-	export LIBS="-Wl,--no-as-needed -ldl"
-	ENABLE_64BIT="--enable-64bit"
-	ENABLE_PARALLEL="--enable-parallel"
-	WITH_MPI="--with-mpi=$MPI"
-    else
-	do_test=0
-    fi
 
 ###########################################
 #   _   _   _   _   _   _
@@ -156,32 +234,6 @@ elif [[ $UNAME == "platypus" ]]; then
 #
 ###########################################
 
-elif [[ $UNAME == "moohan.ad.hdfgroup.org" ]]; then
-#jam gnu , serial
-    if [[ $TEST_NO == 1 ]]; then
-     # export CC="/usr/hdf/bin/gcc52/gcc"
-    #  export FC="/usr/hdf/bin/gcc52/gfortran"
-	export CC="gcc"
-	export FC="gfortran"
-	export FLIBS="-Wl,--no-as-needed -ldl"
-	export LIBS="-Wl,--no-as-needed -ldl"
-	HDF_DIR="/mnt/scr1/pre-release/hdf5/vdev/moohan"
-	TEST_SZIP=`grep -iq "szip" $HDF_DIR/bin/h5cc;echo $?`
-	if [[ $TEST_SZIP == 0 ]]; then
-	    SZIP=`cat $HDF_DIR/bin/h5cc | grep "H5BLD_LDFLAGS=" | sed -e 's/.*H5BLD_LDFLAGS=" -L\(.*\) ".*/\1/'`
-	    ENABLE_SZIP="--with-szip=$SZIP/libsz.a"
-	fi
-      #SZIP=`cat $HDF_DIR/bin/h5cc | grep "H5BLD_LDFLAGS=" | sed -e 's/.*H5BLD_LDFLAGS=" -L\(.*\) ".*/\1/'`
-      #TEST_SZIP=`grep -i "szip" $HDF_DIR/bin/h5cc`
-      #if [[ $TEST_SZIP == 0 ]]; then
-      #  SZIP=`cat $HDF_DIR/bin/h5cc | grep "H5BLD_LDFLAGS=" | sed -e 's/.*H5BLD_LDFLAGS=" -L\(.*\) ".*/\1/'`
-      #  ENABLE_SZIP="--with-szip=$SZIP/libsz.a"
-      #fi
-      #ENABLE_SZIP="--with-szip=$SZIP/libsz.a"
-      #ENABLE_64BIT="--enable-64bit"
-    else
-	do_test=0
-    fi
 ##########################################
 #   _   _   _   _   _   _   _  
 #  / \ / \ / \ / \ / \ / \ / \ 
@@ -190,45 +242,6 @@ elif [[ $UNAME == "moohan.ad.hdfgroup.org" ]]; then
 #
 ##########################################
 
-elif [[ $UNAME ==  "ostrich" ]]; then
-    #gnu 4.4.7, serial
-    if [[ $TEST_NO == 1 ]]; then
-	export CC="gcc"
-	export FC="gfortran"
-	export FLIBS="-Wl,--no-as-needed -ldl"
-	export LIBS="-Wl,--no-as-needed -ldl"
-	ENABLE_64BIT="--enable-64bit"
-	HDF_DIR="/mnt/scr1/pre-release/hdf5/vdev/ostrich"
-	SZIP=`cat $HDF_DIR/bin/h5cc | grep "H5BLD_LDFLAGS=" | sed -e 's/.*H5BLD_LDFLAGS=" -L\(.*\) ".*/\1/'`
-	ENABLE_SZIP="--with-szip=$SZIP/libsz.a"
-    #xlf, serial
-    elif [[ $TEST_NO == 2 ]]; then
-	export CC="/opt/xl/xlf15.1_xlc13.1/xlc"
-	export FC="/opt/xl/xlf15.1_xlc13.1/xlf"
-	export FLIBS=""
-	export LIBS=""
-	ENABLE_64BIT="--enable-64bit"
-	HDF_DIR="/mnt/scr1/pre-release/hdf5/vdev/ostrich-xl"
-	TEST_SZIP=`grep -iq "szip" $HDF_DIR/bin/h5cc;echo $?`
-	if [[ $TEST_SZIP == 0 ]]; then
-	    SZIP=`cat $HDF_DIR/bin/h5cc | grep "H5BLD_LDFLAGS=" | sed -e 's/.*H5BLD_LDFLAGS=" -L\(.*\) ".*/\1/'`
-	    ENABLE_SZIP="--with-szip=$SZIP/libsz.a"
-	fi
-    elif [[ $TEST_NO == 3 ]]; then
-	export CC="/opt/xl/xlf15.1_xlc13.1/xlc"
-	export FC="/opt/xl/xlf15.1_xlc13.1/xlf"
-	export FLIBS=""
-	export LIBS=""
-	HDF_DIR="/mnt/scr1/pre-release/hdf5/vdev/ostrich-xl"
-	TEST_SZIP=`grep -iq "szip" $HDF_DIR/bin/h5cc;echo $?`
-	if [[ $TEST_SZIP == 0 ]]; then
-	    SZIP=`cat $HDF_DIR/bin/h5cc | grep "H5BLD_LDFLAGS=" | sed -e 's/.*H5BLD_LDFLAGS=" -L\(.*\) ".*/\1/'`
-	    ENABLE_SZIP="--with-szip=$SZIP/libsz.a"
-	fi
-    else
-	do_test=0
-    fi
-    
 ##########################################
 #   _   _   _  
 #  / \ / \ / \ 
@@ -236,24 +249,7 @@ elif [[ $UNAME ==  "ostrich" ]]; then
 #  \_/ \_/ \_/ 
 #
 ##########################################
-elif [[ $UNAME ==  "emu" ]]; then
-    make_bin="gmake"
-    HDF_DIR="/mnt/scr1/pre-release/hdf5/vdev/emu64"
-    SZIP=`cat $HDF_DIR/bin/h5cc | grep "H5BLD_LDFLAGS=" | sed -e 's/.*H5BLD_LDFLAGS=" -L\(.*\) ".*/\1/'`
-    ENABLE_SZIP="--with-szip=$SZIP/libsz.a"
-    export CC="cc"
-    export FC="f90"
-    export FLIBS=""
-    export FCFLAGS="-m64"
-    export CFLAGS="-m64"
-    export LIBS=""
-    if [[ $TEST_NO == 1 ]]; then
-	ENABLE_64BIT=""
-    elif [[ $TEST_NO == 2 ]]; then
-	ENABLE_64BIT="--enable-64bit"
-    else
-	do_test=0
-    fi
+
 
 ##########################################
 #   _   _   _   _   _   _   _  
@@ -262,53 +258,28 @@ elif [[ $UNAME ==  "emu" ]]; then
 #  \_/ \_/ \_/ \_/ \_/ \_/ \_/
 #
 ##########################################
-elif [[ $UNAME ==  "osx1010test.ad.hdfgroup.org" ]]; then
-    if [[ $TEST_NO == 1 ]]; then
-	export CC="gcc"
-	export FC="gfortran"
-	export FLIBS="-Wl,-ldl"
-	export LIBS="-Wl,-ldl"
-	HDF_DIR="/mnt/scr1/pre-release/hdf5/vdev/osx1010test"
-	SZIP=`cat $HDF_DIR/bin/h5cc | grep "H5BLD_LDFLAGS=" | sed -e 's/.*H5BLD_LDFLAGS=" -L\(.*\) ".*/\1/'`
-	ENABLE_SZIP="--with-szip=$SZIP/libsz.a"
-    elif [[ $TEST_NO == 2 ]]; then
-	export CC="icc"
-	export FC="ifort"
-	export FLIBS=""
-	export LIBS=""
-	HDF_DIR="/mnt/scr1/pre-release/hdf5/vdev/osx1010test-intel"
-	TEST_SZIP=`grep -iq "szip" $HDF_DIR/bin/h5cc;echo $?`
-	if [[ $TEST_SZIP == 0 ]]; then
-	    SZIP=`cat $HDF_DIR/bin/h5cc | grep "H5BLD_LDFLAGS=" | sed -e 's/.*H5BLD_LDFLAGS=" -L\(.*\) ".*/\1/'`
-	    ENABLE_SZIP="--with-szip=$SZIP/libsz.a"
-	fi
-    else
-	do_test=0
-    fi
-fi
+
 
 if [[ $do_test != 0 ]]; then  
 
     mkdir test.$TEST_NO
     cd test.$TEST_NO
-    if [[ $HDF_DIR == 0 ]]; then
-	mkdir trunk; cd trunk
-	HDF_DIR="$PWD/hdf5"
-	/home/brtnfld/hdf5/trunk/configure $ENABLE_PARALLEL --disable-fortran --disable-hl; $make_bin -j 8 > result.txt 2>&1; $make_bin install
-	cd ..
-    fi
+#    if [[ $HDF_DIR == 0 ]]; then
+#	mkdir trunk; cd trunk
+#	HDF_DIR="$PWD/hdf5"
+#	/home/brtnfld/hdf5/trunk/configure $ENABLE_PARALLEL --disable-fortran --disable-hl; $make_bin -j 8 > result.txt 2>&1; $make_bin install
+#	cd ..
+#    fi
     git clone -b develop https://github.com/CGNS/CGNS.git
     
     cd CGNS/src
     ./configure \
-	--with-hdf5=$HDF_DIR \
-	--with-fortran=yes $ENABLE_PARALLEL \
-	--enable-lfs $ENABLE_64BIT $WITH_MPI $ENABLE_SZIP \
+	$WITH_HDF5 $WITH_FORTRAN $ENABLE_PARALLEL $ENABLE_64BIT $ENABLE_LFS \
+ 	$ENABLE_LEGACY $ENABLE_SCOPE $ENABLE_LFS $ENABLE_SZIP $ENABLE_DEBUG \
 	--disable-shared \
-	--enable-debug \
-	--with-zlib \
 	--disable-cgnstools \
 	--disable-x
+
     status=$?
     if [[ $status != 0 ]]; then
 	echo "CGNS CONFIGURE #FAILED"
@@ -332,36 +303,9 @@ fi
 #rm -fr test.$TEST_NO
 
 #tail -n 100 results.*
-
-do_test=1
-ENABLE_64BIT=""  
+do_test=0
 if [ -d "test.$TEST_NO" ]; then
     cd test.$TEST_NO
-    ENABLE_SZIP="OFF"
-    if [[ $UNAME ==  "jam" ]]; then
-#jam gnu 5.2, serial
-	CC="/usr/hdf/bin/gcc52/gcc"
-	FC="/usr/hdf/bin/gcc52/gfortran"
-	HDF_DIR="$PWD/trunk/hdf5"
-	#CGNS="$PWD/CGNS"
-	TEST_SZIP=`grep -iq "szip" $HDF_DIR/bin/h5cc;echo $?`
-	if [[ $TEST_SZIP == 0 ]]; then
-	    SZIP=`cat $HDF_DIR/bin/h5cc | grep "H5BLD_LDFLAGS=" | sed -e 's/.*H5BLD_LDFLAGS=" -L\(.*\) ".*/\1/'`
-	    ENABLE_SZIP="ON -D SZIP_LIBRARY:PATH=$SZIP/libsz.a"
-	fi
-    elif [[ $UNAME == "platypus" ]]; then
-	CC="gcc"
-	FC="gfortran"
-	HDF_DIR="/mnt/scr1/pre-release/hdf5/vdev/platypus"
-	#CGNS="$PWD/CGNS"
-	TEST_SZIP=`grep -iq "szip" $HDF_DIR/bin/h5cc;echo $?`
-	if [[ $TEST_SZIP == 0 ]]; then
-	    SZIP=`cat $HDF_DIR/bin/h5cc | grep "H5BLD_LDFLAGS=" | sed -e 's/.*H5BLD_LDFLAGS=" -L\(.*\) ".*/\1/'`
-	    ENABLE_SZIP="ON -D SZIP_LIBRARY:PATH=$SZIP/libsz.a"
-	fi
-    else
-	do_test=0
-    fi
     
     if [[ $do_test != 0 ]]; then
 
@@ -371,6 +315,21 @@ if [ -d "test.$TEST_NO" ]; then
 	mkdir CGNS_build
 	cd CGNS_build
 
+	echo "cmake \
+	    -D CMAKE_C_COMPILER:PATH=$CC \
+	    -D CMAKE_C_FLAGS:STRING=\"-g\" \
+	    -D CMAKE_Fortran_FLAGS:STRING=\"-g\" \
+	    -D CMAKE_BUILD_TYPE:STRING=\"Debug\" \
+	    -D CMAKE_Fortran_COMPILER:PATH=$FC \
+	    -D CGNS_BUILD_SHARED:BOOL=OFF \
+	    -D CGNS_USE_SHARED:BOOL=OFF \
+	    -D CMAKE_STATIC_LINKER_FLAGS:STRING=\"\" \
+	    -D CGNS_ENABLE_TESTS:BOOL=ON \
+	    -D CGNS_BUILD_CGNSTOOLS:BOOL=OFF \
+	    -D CGNS_ENABLE_SCOPING:BOOL=OFF \
+	    -D CMAKE_INSTALL_PREFIX:PATH=\"./\" $CMAKE_EXE_LINKER_FLAGS $CGNS_ENABLE_PARALLEL $CGNS_ENABLE_LFS $CGNS_ENABLE_HDF5 $CGNS_ENABLE_FORTRAN $CGNS_ENABLE_64BIT $CGNS"
+
+
 	cmake \
 	    -D CMAKE_C_COMPILER:PATH=$CC \
 	    -D CMAKE_C_FLAGS:STRING="-g" \
@@ -379,22 +338,12 @@ if [ -d "test.$TEST_NO" ]; then
 	    -D CMAKE_Fortran_COMPILER:PATH=$FC \
 	    -D CGNS_BUILD_SHARED:BOOL=OFF \
 	    -D CGNS_USE_SHARED:BOOL=OFF \
-	    -D CGNS_ENABLE_64BIT:BOOL=ON \
-	    -D HDF5_NEED_ZLIB:BOOL=ON \
-	    -D HDF5_NEED_SZIP:BOOL=$ENABLE_SZIP \
-	    -D CMAKE_EXE_LINKER_FLAGS:STRING="-Wl,--no-as-needed -ldl" \
 	    -D CMAKE_STATIC_LINKER_FLAGS:STRING="" \
-	    -D CGNS_ENABLE_HDF5:BOOL=ON \
 	    -D CGNS_ENABLE_TESTS:BOOL=ON \
-	    -D CGNS_ENABLE_LFS:BOOL=OFF \
 	    -D CGNS_BUILD_CGNSTOOLS:BOOL=OFF \
-	    -D HDF5_LIBRARY:STRING=$HDF_DIR/lib/libhdf5.a \
-	    -D HDF5_INCLUDE_PATH:PATH=$HDF_DIR/include \
 	    -D CGNS_ENABLE_SCOPING:BOOL=OFF \
-	    -D CGNS_ENABLE_FORTRAN:BOOL=ON \
-	    -D CGNS_ENABLE_PARALLEL:BOOL=OFF \
-	    -D CMAKE_INSTALL_PREFIX:PATH="./" \
-	    $CGNS 
+	    -D CMAKE_INSTALL_PREFIX:PATH="./" $CMAKE_EXE_LINKER_FLAGS $CGNS_ENABLE_PARALLEL $CGNS_ENABLE_LFS $CGNS_ENABLE_HDF5 $CGNS_ENABLE_FORTRAN $CGNS_ENABLE_64BIT $CGNS 
+	    
         status=$?
         if [[ $status != 0 ]]; then
 	   echo "CGNS CONFIGURE #FAILED"
