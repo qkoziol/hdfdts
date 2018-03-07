539c4acad45446cf8844f2e0a0b2d08dfebb3e7f
lrknox <lrknox>
2018-03-07 11:42:34 -0600

    Add scripts to run make check-s and make check-p as batch jobs on edison at Nersc. Move old batch scripts to obsolete directory. Add script to create directories for release.

---
+++ batch-scripts/makecheck-p.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export NPROCS=12
+make -i check-p >& /global/homes/l/lrknox/source/hdf5/builddir/makecheck-p.out
+
---
+++ batch-scripts/makecheck-s.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+make -i -j 24 check-s >& /global/homes/l/lrknox/source/hdf5/builddir/makecheck-s.out
+
