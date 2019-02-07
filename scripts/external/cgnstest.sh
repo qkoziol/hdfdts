eae8b52a8945f798d02ec55d7948030e0089a9db
M. Scot Breitenfeld <brtnfld@hdfgroup.org>
2019-02-07 16:39:12 -0600

    fixed osx1011test with intel

--- scripts/external/cgnstest.sh
+++ scripts/external/cgnstest.sh
@@ -171,8 +171,12 @@ elif [[ $TEST_COMPILER == gcc* ]]; then
     fi
 
 elif [[ $TEST_COMPILER == "intel" ]]; then
-   if [[ $UNAME == "osx1011test" ]];then
-      /opt/intel/compilers_and_libraries_2016.2.146/mac/bin/compilervars.sh -arch ia32 -platform mac
+
+    if [[ $OSTYPE == "darwin"* ]];then
+       HOSTNAME=`hostname -s`
+       if [[ $HOSTNAME == "osx1011test" ]];then
+          /opt/intel/compilers_and_libraries_2016.2.146/mac/bin/compilervars.sh -arch ia32 -platform mac
+       fi
     fi
     export CC="icc"
     export FC="ifort"
