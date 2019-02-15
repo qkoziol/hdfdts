6040b5b25b53a83ff4bd12ef51b7429f7d473243
Scot Breitenfeld <brtnfld@hdfgroup.org>
2019-02-15 15:27:18 -0700

    misc.

--- scripts/external/nnsa.sh
+++ scripts/external/nnsa.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -l
 
 HDF5_VER="1.11.4"
 
@@ -30,8 +30,12 @@ rm -rf hdf5-$HDF5_VER build hdf5.log Failed*
 sleep 1
 git clone https://git@bitbucket.hdfgroup.org/scm/hdffv/hdf5.git hdf5-$HDF5_VER
 sleep 1
+ln -s hdf5-$HDF5_VER/config/cmake/scripts/CTestScript.cmake .
+ln -s hdf5-$HDF5_VER/config/cmake/scripts/HDF5config.cmake .
+ln -s hdf5-$HDF5_VER/config/cmake/scripts/HDF5options.cmake .
 
 # CROSS-COMPILATION
+# TODO:: INPUT TO SCRIPT
 CCOMP=""
 #CCOMP="KNL"
 KNL="false"
@@ -40,15 +44,21 @@ if [[ $CCOMP == "KNL" ]];then
     KNL="true"
 fi
 
+# Get the curent PrgEnv module setting
 module list &> out
 PRGENV_TYPE=`grep -i PrgEnv out | sed -e 's/.*PrgEnv-\(.*\)\/.*/\1/'`
 
 if [[ $UNAME == "mutrino" ]];then
     module unload craype-hugepages2M
     module load cmake
+# unload the current PrgEnv and compiler assoicated with PrgEnv
     module unload PrgEnv-$PRGENV_TYPE
     module unload $PRGENV_TYPE
+# the modules to test
+#   the PrgEnv to test
     MPI_MOD="PrgEnv-gnu/6.0.4 PrgEnv-intel/6.0.4"
+#   the Compiler to switch to:
+#    Format: <number of compiler versions to check> <compiler type> <list of compiler versions (modules) ... repeat) 
     CC_MOD=(2 gcc gcc/4.9.3 gcc/7.2.0 2 intel intel/16.0.3 intel/18.0.2)
     HPC="sbatch"
 fi
@@ -75,10 +85,11 @@ for mpi_mod in $MPI_MOD; do
 
 #    module list
 
-    echo "timeout 2h ctest . -S HDF5config.cmake,SITE_BUILDNAME_SUFFIX=\"$mpi_mod $cc_mod\",HPC=\"$HPC\",MPI=\"true\",KNL=\"$KNL\",BUILD_GENERATOR=Unix -C Release -V -O hdf5.log"
-#timeout 2h ctest . -S HDF5config.cmake,SITE_BUILDNAME_SUFFIX="$mpi_mod",HPC="$HPC",MPI="true",KNL="$KNL",BUILD_GENERATOR=Unix -C Release -V -O hdf5.log
+    echo "timeout 3h ctest . -S HDF5config.cmake,SITE_BUILDNAME_SUFFIX=\"$mpi_mod $cc_mod\",HPC=\"$HPC\",MPI=\"true\",KNL=\"$KNL\",BUILD_GENERATOR=Unix -C Release -VV -O hdf5.log"
+    timeout 3h ctest . LOCAL_SUBMIT=true MODEL="HPC" -S HDF5config.cmake,SITE_BUILDNAME_SUFFIX="$mpi_mod",HPC="$HPC",MPI="true",KNL="$KNL",BUILD_GENERATOR=Unix -C Release -VV -O hdf5.log
 
     module unload $mpi_mod
     module unload $cc_mod
+    rm -fr build
   done
 done
