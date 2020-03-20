f336797e0eba29d0a90711a6ac12d1aab0a7533a
M. Scot Breitenfeld <brtnfld@hdfgroup.org>
2020-03-20 10:45:50 -0500

    switched branch to test

--- scripts/external/cgnstest.sh
+++ scripts/external/cgnstest.sh
@@ -48,7 +48,7 @@ CGNS_SRC=$BASEDIR/current/CGNS
 
 # set to change to testing a different branch (default is develop)
 #BRANCH="master"
-BRANCH="CGNS-179"
+BRANCH="gsjaardema-patch-2"
 
 # lower case OSTYPE
 OSTYPE=$(echo "$OSTYPE" | tr '[:upper:]' '[:lower:]')
