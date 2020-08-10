e3441e85a21f44f746193be56873f44ac0c4c3eb
M. Scot Breitenfeld <brtnfld@hdfgroup.org>
2020-08-10 00:13:56 -0500

    test CGNS-179

--- scripts/external/cgnstest.sh
+++ scripts/external/cgnstest.sh
@@ -48,8 +48,7 @@ CGNS_SRC=$BASEDIR/current/CGNS
 
 # set to change to testing a different branch (default is develop)
 #BRANCH="master"
-BRANCH="Experimental_X4-X8_hdf5"
-#BRANCH="CGNS-179"
+BRANCH="CGNS-179"
 
 # lower case OSTYPE
 OSTYPE=$(echo "$OSTYPE" | tr '[:upper:]' '[:lower:]')
